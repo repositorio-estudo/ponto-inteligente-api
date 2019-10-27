d56ffb72c0a8fff6f462deeb54b58835bfcfd542
Rodrigo <rodrigo.coimbra@compasso.com.br>
2019-10-27 17:23:18 -0300

    Correção da class test

--- src/test/java/com/coimbra/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/coimbra/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PontoInteligenteApplicationTests {
+public class PontoInteligenteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
